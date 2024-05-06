7fb5d867925937854118bcf270b45b3104a33609
awesome.meow <160415319+cawesomemeow@users.noreply.github.com>
2024-05-06 11:18:50 +0200

    Create scritp.js

---
+++ docs/scritp.js
@@ -0,0 +1,10 @@
+// Optional: Add smooth scrolling behavior
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
