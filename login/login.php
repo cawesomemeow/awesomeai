7878cf7aa0e53150d9ba06f757bb64c22938a268
awesome.meow <160415319+cawesomemeow@users.noreply.github.com>
2024-05-23 10:08:38 +0200

    Update login.php

--- login/login.php
+++ login/login.php
@@ -6,19 +6,21 @@ $valid_users = [
 ];
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $username = $_POST['username'];
-    $email = $_POST['email'];
-    $password = $_POST['password'];
+    // Überprüfen, ob alle erwarteten POST-Daten gesetzt sind
+    if (isset($_POST['username'], $_POST['email'], $_POST['password'])) {
+        $username = htmlspecialchars($_POST['username']);
+        $email = htmlspecialchars($_POST['email']);
+        $password = htmlspecialchars($_POST['password']);
 
-    if (isset($valid_users[$username])) {
-        if ($valid_users[$username]["password"] === $password && $valid_users[$username]["email"] === $email) {
+        // Überprüfen, ob der Benutzername existiert und die Anmeldedaten stimmen
+        if (isset($valid_users[$username]) && $valid_users[$username]["password"] === $password && $valid_users[$username]["email"] === $email) {
             header("Location: free-domain.html");
             exit();
         } else {
             echo "Ungültige Anmeldedaten!";
         }
     } else {
-        echo "Ungültige Anmeldedaten!";
+        echo "Bitte füllen Sie alle Felder aus!";
     }
 }
 ?>
